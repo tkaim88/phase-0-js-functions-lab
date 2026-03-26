20aba6c19aa6c6b5b58f1a52571d71dcdbf07f9f
Your GitHub Username <your-email@example.com>
2026-03-26 09:24:20 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 
+function calculateTax(amount) {
+  return amount * 0.1;
+}
 
+function convertToUpperCase(text) {
+  return text.toUpperCase();
+}
 
+function findMaximum(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  }
+  return num2;
+}
 
+function isPalindrome(word) {
+  let reversed = word.split("").reverse().join("");
+  return word === reversed;
+}
 
-// This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  let discount = originalPrice * (discountPercentage / 100);
+  return originalPrice - discount;
+}
+
+module.exports = {
+  calculateTax,
+  convertToUpperCase,
+  findMaximum,
+  isPalindrome,
+  calculateDiscountedPrice
+};
